b1621ba7afbf73915ce2d70a6aa4c39f10d1d122
Valentyn Vynogradskyi <vinogradskiy.v.a@gmail.com>
2015-06-21 16:19:08 +0200

    TryCommit4

--- DataArt.Test/Controllers/OperationsController.cs
+++ DataArt.Test/Controllers/OperationsController.cs
@@ -1,5 +1,8 @@
-﻿using System.Web.Mvc;
+﻿using System;
+using System.Web.Mvc;
 using DataArt.Test.Core.Abstract;
+using DataArt.Test.Core.Concrete;
+using DataArt.Test.Core.Domain;
 
 namespace DataArt.Test.Controllers
 {
@@ -22,13 +25,29 @@ namespace DataArt.Test.Controllers
         [HttpGet]
         public ActionResult Balance()
         {
-            return View();
+            var userId = (int)Session[Strings.UserId];
+            var user = _service.Balance(userId);
+            return View(user);//todo: in real project will put viewmodel here
         }
 
         [HttpGet]
         public ActionResult GetMoney()
         {
             return View();
+        }
+
+        [HttpPost]
+        public ActionResult GetMoney(int amount)
+        {
+            var userId = (int) Session[Strings.UserId];
+            var operation = _service.GetMoney(userId, amount);
+            return RedirectToAction("OperationResult", operation);
+        }
+
+        [HttpGet]
+        public ActionResult OperationResult(Operation operation)
+        {
+            return View(operation);
         }
     }
 
---
+++ DataArt.Test/Models/GetMoneyViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DataArt.Test.Models
+{
+    public class GetMoneyViewModel
+    {
+        [RegularExpression(@"^\d+$", ErrorMessage = "Please enter proper amount.")]
+        public string Amount { get; set; } 
+    }
+}
